669147f77bd924fb6a36a1552761ec9c004558a1
suraj <suraj.dubey@hotmail.com>
2016-07-20 21:45:04 +0530

    no manual pin enter for pairing

--- app/src/main/java/com/bluetoothpair/android/MainActivity.java
+++ app/src/main/java/com/bluetoothpair/android/MainActivity.java
@@ -13,6 +13,7 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import java.io.IOException;
+import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.Set;
 
@@ -23,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
     private BluetoothAdapter bluetoothAdapter;
     private ListView bluetoothDeviceList;
     private ArrayAdapter<String> bluetoothArrayAdapter;
+    private String pairingRequest = "android.bluetooth.device.action.PAIRING_REQUEST";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,38 +39,50 @@ public class MainActivity extends AppCompatActivity {
         Intent bluetoothIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
         startActivityForResult(bluetoothIntent, 0);
 
-        String pairingRequest = "android.bluetooth.device.action.PAIRING_REQUEST";
+
         registerReceiver(bluetoothDeviceFoundReceiver, new IntentFilter(BluetoothDevice.ACTION_FOUND));
         bluetoothAdapter.startDiscovery();
 
     }
 
-    public final BroadcastReceiver bluetoothDeviceFoundReceiver = new BluetoothDeviceFoundReceiver() {
+    public final BroadcastReceiver bluetoothDeviceFoundReceiver = new BroadcastReceiver() {
 
         @Override
         public void onReceive(Context context, Intent intent) {
             String action = intent.getAction();
             Log.d("bluetooth", action);
-            if(BluetoothDevice.ACTION_FOUND.equals(action)) {
+            if(action.equals(BluetoothDevice.ACTION_FOUND)) {
                 BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
                 device.createBond();
                 bluetoothArrayAdapter.add(device.getName() + " " + device.getAddress());
                 bluetoothArrayAdapter.notifyDataSetChanged();
 
                 //get pin to be entered
-                int pin = intent.getIntExtra("android.bluetooth.device.extra.PAIRING_KEY", 0);
+                int pin = intent.getIntExtra("android.bluetooth.device.extra.PAIRING_KEY", 1234);
                 Log.d("bluetooth", "Pin is " + pin);
-                Log.d("bluetooth", "Bounded " + device.getName());
 
                 byte[] pinBytes;
 
+                String TAG = "bluetooth";
+
                 try {
                     pinBytes = ("" + pin).getBytes("UTF-8");
+//                    Method m = device.getClass().getMethod("setPin", byte[].class);
+//                    m.invoke(device, pinBytes);
+//                    Log.d(TAG, "Success to add the PIN.");
+//                    device.getClass().getMethod("setPairingConfirmation", boolean.class).invoke(device, true);
+//
+//                    Log.d(TAG, "Success to setPairingConfirmation.");
+
                     device.setPin(pinBytes);
                     device.setPairingConfirmation(true);
                 }
                 catch(IOException e) {
                     Log.d("bluetooth", "unhandled IOException");
+                }
+                catch (Exception e) {
+                    Log.e(TAG, e.getMessage());
+                    e.printStackTrace();
                 }
             }
         }
